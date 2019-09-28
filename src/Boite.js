2fd33a30b4be3e3d34d924c28668442fa056e89b
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2019-09-28 10:18:08 -0400

    initial commit

---
+++ src/Boite.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Boite extends React.Component {
+
+  render() {
+
+    return (
+      <p>Ma boite!!!</p>
+    );
+
+  }
+
+}
+
+export default Boite;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Boite from './Boite';
+
+ReactDOM.render(
+  <Boite />,
+  document.getElementById('container')
+);
